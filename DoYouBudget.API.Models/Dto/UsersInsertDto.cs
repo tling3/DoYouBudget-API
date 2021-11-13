52f3eac11f9d8c8f02f0dcf85d4df76223bf2111
Tim Ling <tling3@yahoo.com>
2021-11-13 14:12:25 -0800

    added entity framework and users get all endpoint to test

---
+++ DoYouBudget.API.Models/Dto/UsersInsertDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DoYouBudget.API.Models.Dto
+{
+    public class UsersInsertDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
---
+++ DoYouBudget.API/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using DoYouBudget.API.Data.Interfaces;
+using DoYouBudget.API.Models.Domain;
+using DoYouBudget.API.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DoYouBudget.API.Controllers
+{
+    [Route("api/users")]
+    [Produces("application/json")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUsersRepo _repository;
+        private readonly IMapper _mapper;
+
+        public UsersController(IUsersRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        // GET ALL api/attendance
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UsersReadDto>>> GetUsers()
+        {
+            IEnumerable<UsersModel> domains = await _repository.GetUsers();
+            if (domains == null)
+                return NotFound();
+            IEnumerable<UsersReadDto> dtos = _mapper.Map<IEnumerable<UsersReadDto>>(domains);
+            return Ok(dtos);
+        }
+
+    }
+}
+
+
+
